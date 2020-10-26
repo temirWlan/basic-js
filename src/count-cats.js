19db626909b279988cfb293901ccc2c5e717a1a1
Temirlan <temirlan.balguzhin@bk.ru>
2020-10-26 12:54:22 +0600

    implement count-cats and dream-team function

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,6 +1,3 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function countCats(/* matrix */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+module.exports = countCats = matrix => matrix.flat().filter(item => item === '^^').length;
--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,13 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(arr) {
+
+  if (!Array.isArray(arr)) return false;
+
+  return arr.filter(item => typeof item === 'string')
+            .map(str => str.trim())
+            .map(str => str[0].toUpperCase())
+            .sort()
+            .reduce((acc, curr) => acc + curr, '');
 };
+
