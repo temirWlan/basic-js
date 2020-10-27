af7544a3e7d33f2a2ee7cf9947ead04daa046789
Temirlan <temirlan.balguzhin@bk.ru>
2020-10-27 12:36:33 +0600

    feat: solve tasks

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -3,7 +3,13 @@ const CustomError = require("../extensions/custom-error");
 const MODERN_ACTIVITY= 15; 
 const HALF_LIFE_PERIOD= 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function dateSample(sampleActivity) {
+  const num = +sampleActivity;
+  const n = HALF_LIFE_PERIOD / 0.693;
+
+  if (typeof sampleActivity !== 'string' || typeof num !== 'number' || 
+      isNaN(num) || n <= 0 || n > MODERN_ACTIVITY) return false;
+
+
+  return Math.round(Math.log(MODERN_ACTIVITY / num) * n);
 };
--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,33 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+  let { 
+    repeatTimes, // повторы
+    separator, // разделитель
+    addition, // строка к каждому повторению
+    additionRepeatTimes, // повторы addition
+    additionSeparator // разделитель для addition
+  } = options;
+
+  const arr = [];
+  const additionArr = [];
+
+  separator = separator ? separator : '+';
+  additionSeparator = additionSeparator ? additionSeparator : '|';
+  repeatTimes = repeatTimes ? repeatTimes : 1;
+  additionRepeatTimes = additionRepeatTimes ? additionRepeatTimes : 1;
+
+  for (let i = 0; i < repeatTimes; i++) {
+    arr.push(str);
+  }
+
+  for (let i = 0; i < additionRepeatTimes; i++) {
+    additionArr.push(addition);
+  }
+
+  const additionStr = additionArr.map(item => item = item === null ? `${item}` : item).join(additionSeparator);
+  const result = arr.map(item => `${item}${additionStr}`).join(`${separator}`);
+
+  return result;
 };
   
